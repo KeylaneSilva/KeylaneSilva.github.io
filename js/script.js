f24705245e553e2ffd050cf6504ba089d58f6394
Keylane <keylanessilva07@gmail.com>
2021-06-10 02:10:34 -0300

    exit bootstrap

--- js/script.js
+++ js/script.js
@@ -1,23 +1,73 @@
-const imgProjetos = document.querySelectorAll('.img-projetos');
-const cardProjeto = document.querySelectorAll('.cardPro');
-const pProjeto = document.querySelectorAll('.p-projetos');
+// Variaveis
+const cardContainer = document.querySelector('.cardContainer')
+let popup = document.querySelector('.popup')
 
 
-imgProjetos.forEach((img) =>{
-img.onmouseover = () => {
-        img.classList.add('opacidade')
-        p()
+// Array
+const detalhes = [
+    {
+        Proj: "Projeto 1",
+        src:'img/cNetflix.png',
+        p:'Lorem ipsum Exemplo 1'
+    },
+    {
+        Proj: "Projeto 2",
+        src: 'img/cNetflix.png',
+        p:'Lorem ipsum Exemplo 2'
+    },
+    {
+        Proj: "Projeto 3",
+        src:'img/cNetflix.png',
+        p:'Lorem ipsum Exemplo 3'
+    },
+    {
+        Proj: "Projeto 4",
+        src:'img/cNetflix.png',
+        p:'Lorem ipsum Exemplo 4'
+    },
+    {
+        Proj: "Projeto 5",
+        src:'img/cNetflix.png',
+        p:'Lorem ipsum Exemplo 5'
+    },
+    {
+        Proj: "Projeto 6",
+        src:'img/cNetflix.png',
+        p:'Lorem ipsum Exemplo 6'
+    },
+]
 
-    }
-    img.onmouseout = () => {
-        img.classList.remove('opacidade')
-    }  
-})
+// Funções
+exibir()
 
-function p(){
-    pProjeto.forEach((p) => {
-        p.classList.remove('visivel')
+function exibir(){
+    detalhes.forEach(function(card, index){
+        let cardSingle = document.createElement("div")
+        cardSingle.classList.add("banner")
+        cardSingle.innerHTML = `<a onclick="abrir()"><img src="${card.src}" alt=""></a><p>${card.Proj}</p>`
+        cardContainer.append(cardSingle)
     })
 }
 
+function abrir(){
+    let imagem 
+    let bannerImg = document.querySelectorAll(".banner")
+    
+    detalhes.forEach((value, index) => {
+        
+        bannerImg[index].addEventListener('click', ()=>{
+            imagem = detalhes[index]
+            popup.innerHTML = `<div class="img">
+                                    <img src="${imagem.src}" alt="">
+                                </div>
+                                <p>${imagem.p}</p>
+                                <a href="#">GitHub</a>
+                                <a onclick="fechar()">Voltar</a>`
+        })
+    })
+    popup.style.display = "block"
+}
 
+function fechar(){
+    popup.style.display = "none"
+}
